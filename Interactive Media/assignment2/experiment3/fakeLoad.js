c76cf6c9b013285a390621933eaf342988ed1cc3
Olivia Pietralla <pietrallaok@gmail.com>
2023-04-23 14:03:09 +1000

    animation draft

--- Interactive Media/assignment2/experiment3/fakeLoad.js
+++ Interactive Media/assignment2/experiment3/fakeLoad.js
@@ -8,7 +8,7 @@ function loadPageAfterDelay(loadTime, url){
   // hide link
   document.getElementById("load-link").style.display = 'none';
   // show throbber
-  document.getElementById("load-throbber").style.display = 'block';
+  document.getElementById("animation-container").style.display = 'flex';
   /* if you want to add or change anything else once link is clicked below here is where you'd do so */
   /* then the second part is actually following the link using a setTimeout() */
   /*
