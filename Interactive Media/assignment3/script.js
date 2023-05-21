cc6e384552dcde2c2a2fd62b756be2fcb424d47c
Olivia Pietralla <pietrallaok@gmail.com>
2023-05-21 17:18:09 +1000

    buttons on hover

---
+++ Interactive Media/assignment3/script.js
@@ -0,0 +1,16 @@
+// RANDOMNESS //
+
+// randomPosition 
+// absolute
+// left and top 
+// offset - minus the width of the button so that it doesnt go off the screen
+
+// keep something from repeating 
+// shift - removes one item from the array (used name array)
+
+// Create buttons into array - randomIntRange (whole numbers) ((suffleArray))
+
+// add event listening ("click"), function (random Position)
+
+// BUTTON ARRAY
+
