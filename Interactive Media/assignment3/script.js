9ca0524acf95060679d6841ed1f3ff7d221c58fc
Olivia Pietralla <pietrallaok@gmail.com>
2023-05-25 10:41:27 +1000

    showbtn

--- Interactive Media/assignment3/script.js
+++ Interactive Media/assignment3/script.js
@@ -14,3 +14,48 @@
 
 // BUTTON ARRAY
 
+
+const buttonArray = ['StartButton, RoundButton, EnlargeButton, SquareButton, ShrinkButton, MovementButton, ColourButton, InvertButton, SidewaysButton, RotateButton']
+const buttonVisible = [];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+
+const button = buttonArray[getRandomInt(buttonArray.length)];
+document.getElementById(button)
+button.classList.toggle('hidden');
+
+colorButton.addEventListener("click"), function() {
+    buttonVisible.forEach(button => {
+        button.toggle('Colour')
+    });
+}
+
+function ShowBtn() {
+    document.getElementById('RoundButton').style.display = "block";
+}
+
+// COLLAPSIBLE 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+// COLOUR BUTTON
+
+function colour(){
+    document.getElementById("ColourButton").style.backgroundColor = "red";
+}
