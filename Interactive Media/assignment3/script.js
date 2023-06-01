82e9fb1d45432f5394a48b0b981da6716fef2d03
Olivia Pietralla <pietrallaok@gmail.com>
2023-06-01 15:58:46 +1000

    individual functions

--- Interactive Media/assignment3/script.js
+++ Interactive Media/assignment3/script.js
@@ -15,14 +15,6 @@
 // BUTTON ARRAY
 
 
-const buttonArray = ['StartButton, RoundButton, EnlargeButton, SquareButton, ShrinkButton, MovementButton, ColourButton, InvertButton, SidewaysButton, RotateButton']
-const buttonVisible = [];
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-
 // const button = buttonArray[getRandomInt(buttonArray.length)];
 // document.getElementById(button)
 // button.classList.toggle('hidden');
@@ -35,19 +27,28 @@ function getRandomInt(max) {
 
 // BUTTONS APPEAR
 
+
+
 function ShowBtn1() {
     document.getElementById('RoundButton').style.display = "inline";
     document.getElementById('SquareButton').style.display = "inline";
+}
+
+function RoundBtn() {
 
     let buttons = document.getElementsByClassName('changeBtn');
 
     for (let button of buttons) {
-        //button.innerHTML = "test"
-        button.classList.add('RoundBtnClick')
+        button.classList.add('RoundBtnClick');
     }
+}
+
+function SquareBtn() {
+    
+    let buttons = document.getElementsByClassName('changeBtn');
 
     for (let button of buttons) {
-        button.classList.add('SquBtnClick')
+        button.classList.add('SquareBtnClick');
     }
 }
 
@@ -57,6 +58,33 @@ function ShowBtn2() {
     document.getElementById('ShrinkButton').style.display = "inline";
 }
 
+function EnlargeBtn() {
+
+    let buttons = document.getElementsByClassName('changeBtn');
+
+    for (let button of buttons) {
+        button.classList.add('EnlargeBtnClick');
+    }
+}
+
+function ColourBtn() {
+
+    let buttons = document.getElementsByClassName('changeBtn');
+
+    for (let button of buttons) {
+        button.classList.add('ColourBtnClick');
+    }
+}
+
+function ShrinkBtn() {
+
+    let buttons = document.getElementsByClassName('changeBtn');
+
+    for (let button of buttons) {
+        button.classList.add('ShrinkBtnClick');
+    }
+}
+
 function ShowBtn3() {
     document.getElementById('InvertButton').style.display = "inline";
     document.getElementById('DarkModeButton').style.display = "inline";
@@ -64,15 +92,60 @@ function ShowBtn3() {
     document.getElementById('MovementButton').style.display = "inline";
 }
 
-function ShowBtn4() {
-    document.getElementById('OtherButton').style.display = "inline"; 
+function InvertBtn() {
+
+    let buttons = document.getElementsByClassName('changeBtn');
+
+    for (let button of buttons) {
+        button.classList.add('InvertBtnClick');
+    }
 }
 
+function DarkModeBtn() {
 
-function ColourBtn() {
-    document.getElementsByClassName('colour').style.background = "purple";
+    let buttons = document.getElementsByClassName('changeBtn');
+
+    for (let button of buttons) {
+        button.classList.add('DarkModeBtnClick');
+    }
+}
+
+function RotateBtn() {
+
+    let buttons = document.getElementsByClassName('changeBtn');
+
+    for (let button of buttons) {
+        button.classList.add('RotateBtnClick');
+    }
+}
+
+function MovementBtn() {
+
+    let buttons = document.getElementsByClassName('changeBtn');
+
+    for (let button of buttons) {
+        button.classList.add('MovementBtnClick');
+    }
+}
+
+function ShowBtn4() {
+    document.getElementById('OtherButton').style.display = "inline"; 
 }
 
 function ShowTxt() {
     document.getElementById("words").innerHTML = "500 Word Explanation";
 }
+
+function ResetBtn() {
+    
+    let buttons = document.getElementsByClassName('changeBtn');
+
+    for (let button of buttons) {
+        if (button.className.indexOf('hidden') === -1)
+            button.className = 'ResetBtnClick changeBtn';
+        else
+            button.className = 'ResetBtnClick changeBtn hidden';
+    }
+}
+
+// button container not to move when button changes shape
